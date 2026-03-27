f6a167b20c9ccdbc62db3a3084537a1f8cf67808
Angelo Galvao <angelogalvao@gmail.com>
2026-03-26 22:45:52 -0400

    Optical Character Recognition.

---
+++ ai-java/README.md
@@ -0,0 +1,5 @@
+# AI Java projects
+
+This folder contains multiple java projects running some aspect of AI.
+
+1. **fraud-client-inference:** 
---
+++ ai-java/optical-character-recognition/src/main/java/io/github/angelogalvao/example/ai/ScreenShotResource.java
@@ -0,0 +1,43 @@
+package io.github.angelogalvao.example.ai;
+
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.net.URI;
+
+@Path("/extract")
+public class ScreenShotResource {
+
+    @Inject
+    ChatModel chatModel;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String extract() {
+
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from(
+                    """
+                    This is image was reported on a GitHub issue.
+                    
+                    If this is a snippet of Java code, please respond with only the Java code.
+                    
+                    If the lines are numbered, removes them from the output.
+                    If it is not Java code, respond with 'NOT AN IMAGE'
+                    """
+                ),
+                ImageContent.from(URI.create("https://i.postimg.cc/fL6x1MK9/screenshot.png"))
+        );
+
+        ChatResponse response = chatModel.chat(userMessage);
+        return response.aiMessage().text();
+    }
+}
